b6871cb74aa0c64e758e5bbb609c8078d523d4fd
Karan Sharma <karanvatish@gmail.com>
2020-12-28 19:07:46 +0530

    Created Initial project with Create react app and added SASS with homepage files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HomePageComponent from './Components/HomePage/homepage-component';
+
+function App() {
+  return (
+    <div>
+      <HomePageComponent/>
+    </div>
+  );
+}
+
+export default App;
